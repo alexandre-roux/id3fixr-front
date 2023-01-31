b6bb656e5b031127e2f0e920dfe1505fc2a0db2a
Alexandre  Roux <aroux@softbankrobotics.com>
2023-01-31 13:56:57 +0100

    Read ID3 tags

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import "./App.css";
 import {useState} from "react";
 
 function App() {
-  const [selectedFile, setSelectedFile] = useState();
   const [isSelected, setIsSelected] = useState(false);
+  const [selectedFile, setSelectedFile] = useState();
+  const [tags, setTags] = useState()
 
   const changeHandler = (event) => {
-    setSelectedFile(event.target.files[0]);
-    setIsSelected(true);
+    const file = event.target.files[0]
+    setSelectedFile(file);
+
+    window.jsmediatags.read(file, {
+      onSuccess: function (result) {
+        console.log(result.tags);
+        setTags(result.tags)
+        setIsSelected(true);
+      },
+      onError: function (error) {
+        console.log(error);
+      }
+    });
   };
 
   return (
@@ -15,12 +27,29 @@ function App() {
       <input type="file" name="file" onChange={changeHandler}/>
       {isSelected ? (
         <div>
-          <p>Filename: {selectedFile.name}</p>
-          <p>Filetype: {selectedFile.type}</p>
-          <p>Size in bytes: {selectedFile.size}</p>
-          <p>
-            lastModifiedDate: {selectedFile.lastModified}
-          </p>
+          <div>
+            <h1>File info</h1>
+            <p>Filename: {selectedFile.name}</p>
+            <p>Filetype: {selectedFile.type}</p>
+          </div>
+          <div>
+            <h1>ID3 tags</h1>
+            <p>Title: {tags.title}</p>
+            <p>Artist: {tags.artist}</p>
+            <p>Album: {tags.album}</p>
+            <p>Genre: {tags.genre}</p>
+            <p>Year: {tags.year}</p>
+            <p>Track: {tags.track}</p>
+            <img
+              src={`data:${tags.picture.format};base64,${btoa(
+                new Uint8Array(tags.picture.data).reduce(
+                  (data, byte) => data + String.fromCharCode(byte),
+                  ""
+                )
+              )}`}
+              alt={tags.picture.description}
+            />
+          </div>
         </div>
       ) : (
         <p>Select a file to show details</p>
