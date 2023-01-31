b1734ba7aef2ade5d0820010070c3e51b347814d
Alexandre  Roux <aroux@softbankrobotics.com>
2023-01-31 15:07:06 +0100

    Save new tags to file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import {useState} from "react";
+import ID3Writer from "browser-id3-writer"
+import {saveAs} from 'file-saver';
 
 function App() {
   const [isSelected, setIsSelected] = useState(false);
@@ -20,11 +22,37 @@ function App() {
         console.log(error);
       }
     });
+
+    const reader = new FileReader();
+    reader.onload = function () {
+      const arrayBuffer = reader.result;
+      // go next
+      // arrayBuffer of song or empty arrayBuffer if you just want only id3 tag without song
+      const writer = new ID3Writer(arrayBuffer);
+      writer.setFrame('TIT2', 'Home')
+        .setFrame('TPE1', ['Eminem', '50 Cent'])
+        .setFrame('TALB', 'Friday Night Lights')
+        .setFrame('TYER', 2004)
+        .setFrame('TRCK', '6/8')
+        .setFrame('TCON', ['Soundtrack'])
+        .setFrame('TBPM', 128)
+        .setFrame('WPAY', 'https://google.com')
+        .setFrame('TKEY', 'Fbm')
+      writer.addTag();
+      const taggedSongBuffer = writer.arrayBuffer;
+      const blob = writer.getBlob();
+      saveAs(blob, 'song with tags.mp3');
+    };
+    reader.onerror = function () {
+      // handle error
+      console.error('Reader error', reader.error);
+    };
+    reader.readAsArrayBuffer(file);
   };
 
   return (
     <div>
-      <input type="file" name="file" onChange={changeHandler}/>
+      <input type="file" name="file" accept="audio/mpeg" onChange={changeHandler}/>
       {isSelected ? (
         <div>
           <div>
@@ -34,21 +62,21 @@ function App() {
           </div>
           <div>
             <h1>ID3 tags</h1>
-            <p>Title: {tags.title}</p>
-            <p>Artist: {tags.artist}</p>
-            <p>Album: {tags.album}</p>
-            <p>Genre: {tags.genre}</p>
-            <p>Year: {tags.year}</p>
-            <p>Track: {tags.track}</p>
-            <img
+            <p>Title: {tags?.title}</p>
+            <p>Artist: {tags?.artist}</p>
+            <p>Album: {tags?.album}</p>
+            <p>Genre: {tags?.genre}</p>
+            <p>Year: {tags?.year}</p>
+            <p>Track: {tags?.track}</p>
+            {tags.picture && <img
               src={`data:${tags.picture.format};base64,${btoa(
                 new Uint8Array(tags.picture.data).reduce(
                   (data, byte) => data + String.fromCharCode(byte),
                   ""
                 )
               )}`}
-              alt={tags.picture.description}
-            />
+              alt={tags?.picture.description}
+            />}
           </div>
         </div>
       ) : (
