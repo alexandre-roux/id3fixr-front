3f122930c7a5b740b8ff8f26954d8defd30cfad1
Alexandre  Roux <aroux@softbankrobotics.com>
2023-02-01 17:31:36 +0100

    If artist and track name doesn't  exist, use file name

--- src/components/DiscogsSearcher.js
+++ src/components/DiscogsSearcher.js
@@ -1,39 +1,38 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const DiscogsSearcher = ({selectedFile, tags}) => {
-  const [data, setData] = useState()
+const DiscogsSearcher = ({ selectedFile, tags }) => {
+  const [data, setData] = useState();
 
   useEffect(() => {
-    // First, look into the tags
-    const keywords = tags.artist + " " + tags.title
+    let keywords = "";
+
+    if (tags.artist && tags.title) {
+      keywords = tags.artist + " " + tags.title;
+    } else {
+      keywords = selectedFile.name;
+      keywords = keywords.replace(" - ", " ");
+      keywords = keywords.replace(".mp3", "");
+      console.log(keywords);
+    }
 
     const fetchData = async () => {
       try {
         const response = await axios.get("http://localhost:3100/search", {
           params: {
-            keywords: keywords
-          }
+            keywords: keywords,
+          },
         });
         console.log(response.data);
         setData(response.data);
       } catch (error) {
-        console.log(error.response); // contrairement au error.message d'express
+        console.log(error.response);
       }
     };
     fetchData();
-  }, [tags])
+  }, [tags]);
 
-
-  return (
-    <>
-      {data && (
-        <div>
-
-        </div>
-      )}
-    </>
-  );
+  return <>{data && <div></div>}</>;
 };
 
 export default DiscogsSearcher;
