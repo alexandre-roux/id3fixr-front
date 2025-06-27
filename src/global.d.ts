83fdf48a3f6e47207ae50990fb0373dc88d2a977
Alexandre Roux <alexandre.roux@ensc.fr>
2025-06-27 13:17:43 +0200

    Global types and environment const management

---
+++ src/global.d.ts
@@ -0,0 +1,32 @@
+// Interface for the structure of ID3 tags returned by musicmetadata
+interface MusicMetadataResult {
+    title: string;
+    artist: string[];
+    album: string;
+    genre: string[];
+    year: string;
+    track: { no: number; of: number };
+    picture: { format: string; data: number[] }[];
+}
+
+// Declaration for custom elements (ion-icon)
+declare namespace JSX {
+    interface IntrinsicElements {
+        'ion-icon': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+            name: string;
+        };
+    }
+}
+
+
+// Declaration of the musicmetadata function on the global 'window' object
+declare global {
+    interface Window {
+        musicmetadata(
+            file: File,
+            callback: (error: Error | null, result: MusicMetadataResult) => void
+        ): void;
+    }
+}
+
+export {};
